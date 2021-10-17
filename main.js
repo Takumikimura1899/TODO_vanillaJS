1e96e0df2f78e672e6c550cd623c999973c5aec2
Takumikimura141899 <kimura141899@gmail.com>
2021-10-18 07:49:50 +0900

    [add] taskLength

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const taskValue = document.getElementById('task_value');
 const taskSubmit = document.getElementById('task_submit');
 const taskList = document.getElementById('task_list');
+const getElementH2 = document.querySelector('h2');
+const createSpan = document.createElement('span');
+const showSpan = getElementH2.appendChild(createSpan);
 
 const addTasks = (task) => {
   const listItem = document.createElement('li');
@@ -23,9 +26,16 @@ taskSubmit.addEventListener('submit', (e) => {
   console.log(task);
   task && addTasks(task);
   taskValue.value = '';
+  getTasksLength();
 });
 
 const deleteTasks = (deleteButton) => {
   const chosenTask = deleteButton.closest('li');
   taskList.removeChild(chosenTask);
+};
+
+const getTasksLength = () => {
+  const tasks = document.querySelectorAll('li');
+  console.log(tasks.length);
+  showSpan.textContent = ':' + tasks.length;
 };
