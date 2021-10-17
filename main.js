643300a347271a4521abc1fb0e1e1735641a2dca
Takumikimura141899 <kimura141899@gmail.com>
2021-10-17 20:25:34 +0900

    [add] addtasks

---
+++ main.js
@@ -0,0 +1,16 @@
+const taskValue = document.getElementById('task_value');
+const taskSubmit = document.getElementById('task_submit');
+const taskList = document.getElementById('task_list');
+
+const addTasks = (task) => {
+  const listItem = document.createElement('li');
+  const showItem = taskList.appendChild(listItem);
+  showItem.innerHTML = task;
+};
+
+taskSubmit.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = taskValue.value;
+  console.log(task);
+  addTasks(task);
+});
